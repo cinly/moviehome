14df9ddfb21fba05bd2d5abb260a8c50564c2fc6
cinly <cinlyma@gmail.com>
2017-04-04 10:42:25 +0800

    moviehome

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,7 +15,7 @@ class GroupsController < ApplicationController
 
    def update
      if @group.update(group_params)
-       redirect_to groups_path, notice: "Update Success"
+       redirect_to groups_path, notice: "已更新"
      else
        render :edit
      end
@@ -23,7 +23,7 @@ class GroupsController < ApplicationController
 
    def destroy
      @group.destroy
-     redirect_to groups_path, alert: "Group deleted"
+     redirect_to groups_path, alert: "已删除"
    end
 
   def new
@@ -47,9 +47,9 @@ class GroupsController < ApplicationController
 
     if !current_user.is_member_of?(@group)
       current_user.join!(@group)
-      flash[:notice] = "加入本讨论版成功！"
+      flash[:notice] = "已收藏！"
     else
-      flash[:warning] = "你已经是本讨论版成员了！"
+      flash[:warning] = "您已收藏！"
     end
 
     redirect_to group_path(@group)
@@ -60,9 +60,9 @@ class GroupsController < ApplicationController
 
     if current_user.is_member_of?(@group)
       current_user.quit!(@group)
-      flash[:alert] = "已退出本讨论版！"
+      flash[:alert] = "取消收藏！"
     else
-      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+      flash[:warning] = "未收藏，不用取消啦！"
     end
 
     redirect_to group_path(@group)
